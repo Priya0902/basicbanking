68bd589c427915042d5d630af093d41f95f6c589
Priya0902 <lakshmipriya.bellamkonda09@gmail.com>
2021-12-16 11:22:25 +0530

    BankJs

---
+++ banking/Bank.js
@@ -0,0 +1,27 @@
+function Transfer() 
+{
+  
+    var myaccount=parseInt(document.getElementById('PriyaBalance').innerText);
+    var to_acc=document.getElementById('To').value;
+    var transfer_Amount=parseInt(document.getElementById('Amount').value);
+
+if(myaccount>transfer_Amount && transfer_Amount>200)
+{
+    var x=to_acc+"Balance";
+    var to_acc_balance=parseInt(document.getElementById(x).innerHTML);   
+    var  receiver_curr=parseInt(transfer_Amount+to_acc_balance);
+    let myaccount=parseInt(document.getElementById('PriyaBalance').innerText)-transfer_Amount;
+    alert("Your Account Balance is "+myaccount+"\n"+"Transacted Rs."+transfer_Amount+" to "+to_acc);
+    document.getElementById(x).innerHTML=receiver_curr;
+    document.getElementById('PriyaBalance').innerText=myaccount;
+    alert("Continue")
+    document.getElementById('PriyaBalance').innerText=myaccount;
+    window.location="Last.html";
+}
+
+else 
+{
+    alert("Insufficient Balance For the Transaction ")
+}
+
+} 
